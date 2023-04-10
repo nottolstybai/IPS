ef34e17c1d838e1a20262de030968a166ebb5a30
tolstybai <tolstybai@gmail.com>
2023-04-11 02:13:33 +0300

    use networkx

--- verification_module/tree_builder.py
+++ verification_module/tree_builder.py
@@ -1,5 +1,6 @@
 # импортируем модуль json для работы с данными
 import json
+import networkx as nx
 
 # загружаем данные из строки в переменную data
 data = json.loads("""
@@ -9,7 +10,7 @@ data = json.loads("""
     "Text": "Требование 1",
     "Comment": "Комментарий 1",
     "Class": "B",
-    "Parent": 2
+    "Parent": null
   },
   {
     "ID": 2,
@@ -38,47 +39,26 @@ data = json.loads("""
 
 def create_graph(data):
     graph = {}
-    # перебираем все требования в данных
     for req in data:
-        # получаем ID, category и Parent текущего требования
         id = req["ID"]
         category = req["Class"]
         parent = req["Parent"]
-        # если ID еще нет в словаре graph, то создаем пустой список для хранения дочерних узлов
         if id not in graph:
             graph[id] = {"Children": [], "Category": category, "Parent": parent}
-        # если Parent не равен null, то добавляем ID в список дочерних узлов Parent
-        if parent != None:
-            # если Parent еще нет в словаре graph, то создаем пустой список для хранения дочерних узлов
+        if parent is not None:
             if parent not in graph:
                 graph[parent] = {"Children": [], "Category": None, "Parent": None}
-            # добавляем ID в список дочерних узлов Parent
             graph[parent]["Children"].append(id)
-    # возвращаем граф
     return graph
 
 
-def dfs(start, visited, path, graph, cycles):
-    # добавляем текущий узел в список посещенных и путь
-    visited.add(start)
-    path.append(start)
-    # находим все дочерние узлы текущего узла, если они есть в графе
-    if start in graph:
-        for child in graph[start]["Children"]:
-            # если дочерний узел уже посещен, то это цикл
-            if child in visited:
-                # добавляем цикл в список циклов, если еще не добавили
-                cycle = path[path.index(child):]
-                # если цикл состоит из двух элементов, то сортируем его по возрастанию ID
-                if len(cycle) == 2:
-                    cycle.sort()
-                if cycle not in cycles:
-                    cycles.append(cycle)
-            # иначе продолжаем поиск в глубину от дочернего узла
-            else:
-                dfs(child, visited, path, graph, cycles)
-    # удаляем текущий узел из пути
-    path.pop()
+def find_cycles(graph):
+    G = nx.DiGraph()
+    for node in graph:
+        for child in graph[node]["Children"]:
+            G.add_edge(node, child)
+    cycles = list(nx.simple_cycles(G))
+    return cycles
 
 
 def find_self_referencing_nodes(graph):
@@ -90,7 +70,6 @@ def find_self_referencing_nodes(graph):
     return error_nodes
 
 
-# функция для нахождения узлов класса B, ссылающихся на узлы класса F
 def find_B_to_F_nodes(graph):
     """функция для нахождения узлов класса B, ссылающихся на узлы класса F"""
     error_nodes = []
@@ -109,14 +88,8 @@ if __name__ == '__main__':
     print("Создан следующий граф:")
     print(graph)
 
-    cycles = []
-    for node in graph:
-        visited = set()
-        path = []
-        dfs(node, visited, path, graph, cycles)
-
     print("Найдены следующие циклы:")
-    for cycle in cycles:
+    for cycle in find_cycles(graph):
         print(cycle)
 
     print("Самоссылка:")
