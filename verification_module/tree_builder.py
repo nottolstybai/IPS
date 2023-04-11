40a53039ff6285f36ba46ac4af69fb3a8494ce73
tolstybai <tolstybai@gmail.com>
2023-04-11 21:48:22 +0300

    fix bugs: 1) Plot all nodes even simple nodes with no relations 2) fix inheritance_heck function 3) remove function that checked if node references itself 4) when plotting remove all labels and add arrows instead of parent and chil labels, colors responsible for reqs classes

--- verification_module/tree_builder.py
+++ verification_module/tree_builder.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -9,21 +10,21 @@ data = json.loads("""
     "ID": 1,
     "Text": "Требование 1",
     "Comment": "Комментарий 1",
-    "Class": "B",
-    "Parent": 4
+    "Class": "F",
+    "Parent": 2
   },
   {
     "ID": 2,
     "Text": "Требование 2",
     "Comment": "Комментарий 2",
-    "Class": "F",
+    "Class": "B",
     "Parent": 1
   },
   {
     "ID": 3,
     "Text": "Требование 3",
     "Comment": null,
-    "Class": "B",
+    "Class": "F",
     "Parent": 2
   },
   {
@@ -49,6 +50,9 @@ def create_graph(data):
             if parent not in graph:
                 graph[parent] = {"Children": [], "Category": None, "Parent": None}
             graph[parent]["Children"].append(id)
+        if id in graph and graph[id]["Category"] is None and graph[id]["Parent"] is None:
+            graph[id]["Category"] = category
+            graph[id]["Parent"] = parent
     return graph
 
 
@@ -64,36 +68,38 @@ def find_cycles(graph):
 def draw_graph(graph):
     G = nx.DiGraph()
     for node in graph:
-        for child in graph[node]["Children"]:
-            G.add_edge(node, child, Category=graph[node]["Category"])
-        G.nodes[node]["Children"] = graph[node]["Children"]
-        G.nodes[node]["Parent"] = graph[node]["Parent"]
+        G.add_node(node)
+        for child in graph[node]['Children']:
+            G.add_edge(node, child)
+    labels = {}
+    colors = {}
+    for node in graph:
+        category = graph[node]['Category']
+        # Создаем словарь меток id
+        labels[node] = node
+        # Если категория еще не имеет цвета, генерируем случайный цвет
+        if category not in colors:
+            colors[category] = (random.random(), random.random(), random.random())
+    # Создаем список цветов для каждого узла в соответствии с его категорией
+    node_colors = [colors[graph[node]['Category']] for node in graph]
     pos = nx.spring_layout(G)
-    nx.draw_networkx(G, with_labels=True, pos=pos)
-    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=nx.get_edge_attributes(G, "Category"))
-    node_labels = {}
-    for node in G.nodes:
-        node_labels[node] = f"Children: {G.nodes[node]['Children']}\nParent: {G.nodes[node]['Parent']}"
-    nx.draw_networkx_labels(G, pos=pos, labels=node_labels)
+    # Передаем словарь меток id в параметр labels
+    nx.draw(G, pos=pos, with_labels=True, labels=labels, node_color=node_colors, cmap='jet')
+    # Создаем подписи к категориям
+    handles = []
+    for category, color in colors.items():
+        handles.append(plt.scatter([], [], color=color, label=category))
+    plt.legend(handles=handles)
     plt.show()
 
 
-def find_self_referencing_nodes(graph):
-    """функция для нахождения узлов, которые ссылаются на себя"""
-    error_nodes = []
-    for node in graph:
-        if graph[node]["Parent"] == node:
-            error_nodes.append(node)
-    return error_nodes
-
-
-def find_B_to_F_nodes(graph):
+def find_BNodes_to_notBnodes(graph):
     """функция для нахождения узлов класса B, ссылающихся на узлы класса F"""
     error_nodes = []
     for node in graph:
         if graph[node]["Category"] == "B":
             parent = graph[node]["Parent"]
-            if parent is not None and graph[parent]["Category"] == "F":
+            if parent is not None and graph[parent]["Category"] != "B":
                 error_nodes.append(node)
     return error_nodes
 
@@ -109,10 +115,7 @@ if __name__ == '__main__':
     for cycle in find_cycles(graph):
         print(cycle)
 
-    print("Самоссылка:")
-    print(find_self_referencing_nodes(graph))
-
     print("Ошибка наследования:")
-    print(find_B_to_F_nodes(graph))
+    print(find_BNodes_to_notBnodes(graph))
 
     draw_graph(graph)
