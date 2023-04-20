83b21cd7d60d2c299f2a54ef76903ce8543baa56
tolstybai <tolstybai@gmail.com>
2023-04-20 22:19:46 +0300

    add test case connection

--- verification_module/tree_builder.py
+++ verification_module/tree_builder.py
@@ -38,90 +38,116 @@ data = json.loads("""
 """)
 
 
-def create_graph(data):
-    graph = {}
-    for req in data:
-        id = req["ID"]
-        category = req["Class"]
-        parent = req["Parent"]
-        if id not in graph:
-            graph[id] = {"Children": [], "Category": category, "Parent": parent}
-        if parent is not None:
-            if parent not in graph:
-                graph[parent] = {"Children": [], "Category": None, "Parent": None}
-            graph[parent]["Children"].append(id)
-        if id in graph and graph[id]["Category"] is None and graph[id]["Parent"] is None:
-            graph[id]["Category"] = category
-            graph[id]["Parent"] = parent
-    return graph
+class Requirement:
+    def __init__(self, id, category, parent):
+        self.id = id
+        self.category = category
+        self.parent = parent
+        self.tests = []
+        self.children = []
+
+    def add_test(self, test_case):
+        self.tests.append(test_case)
+
+    def __str__(self):
+        return f"Requirement(id={self.id}, category={self.category}, parent={self.parent}, tests={self.tests})"
+
+    def __repr__(self):
+        return f"Requirement(id={self.id}, category={self.category}, parent={self.parent}, tests={self.tests})"
 
 
-def find_cycles(graph):
-    G = nx.DiGraph()
-    for node in graph:
-        for child in graph[node]["Children"]:
-            G.add_edge(node, child, CLASS=graph[node]["Category"])
-    cycles = list(nx.simple_cycles(G))
-    return cycles
+class Graph:
+    def __init__(self):
+        self.nodes = {}
 
+    def add_node(self, node):
+        if node.id not in self.nodes:
+            self.nodes[node.id] = node
 
-def draw_graph(graph):
-    G = nx.DiGraph()
-    labels = {}
-    colors = {}
-    for node in graph:
-        category = graph[node]['Category']
-        labels[node] = node
-        if category not in colors:
-            colors[category] = (random.random(), random.random(), random.random())
-        G.add_node(node)
-    for node in graph:
-      for child in graph[node]['Children']:
-          G.add_edge(node, child)
-    node_colors = [colors[graph[node]['Category']] for node in graph]
-    pos = nx.spring_layout(G, seed=42) # добавить атрибут seed
-    sorted_pos = dict(sorted(pos.items())) # отсортировать словарь pos по ключам
-    nx.draw(G, pos=sorted_pos, with_labels=True, labels=labels, node_color=node_colors, cmap='jet') # использовать sorted_pos и sorted_node_colors
-    handles = []
-    for category, color in colors.items():
-        handles.append(plt.scatter([], [], color=color, label=category))
-    plt.legend(handles=handles)
-    plt.show()
+    def add_edge(self, parent, child):
+        if parent not in self.nodes:
+            self.add_node(Requirement(parent, None, None))
+        if child not in self.nodes:
+            self.add_node(Requirement(child, None, None))
+        self.nodes[parent].children.append(child)
 
+    def create_from_data(self, data):
+        for req in data:
+            id = req["ID"]
+            category = req["Class"]
+            parent = req["Parent"]
+            self.add_node(Requirement(id, category, parent))
+            if parent is not None:
+                self.add_edge(parent, id)
+            if id in self.nodes and self.nodes[id].category is None and self.nodes[id].parent is None:
+                self.nodes[id].category = category
+                self.nodes[id].parent = parent
+        return self
 
-def find_BNodes_to_notBnodes(graph):
-    """функция для нахождения узлов класса B, ссылающихся на узлы класса F"""
-    error_nodes = []
-    for node in graph:
-        if graph[node]["Category"] == "B":
-            parent = graph[node]["Parent"]
-            if parent is not None and graph[parent]["Category"] != "B":
+    def find_BNodes_to_notBnodes(self):
+        """метод для нахождения узлов класса B, ссылающихся на узлы класса F"""
+        error_nodes = []
+        for node in self.nodes:
+            if self.nodes[node].category == "B":
+                parent = self.nodes[node].parent
+                if parent is not None and self.nodes[parent].category != "B":
+                    error_nodes.append(node)
+        return error_nodes
+
+    def find_alone_nodes(self):
+        """метод для нахождения одиночных узлов"""
+        error_nodes = []
+        for node in self.nodes:
+            if len(self.nodes[node].children) == 0 and self.nodes[node].parent is None:
                 error_nodes.append(node)
-    return error_nodes
+        return error_nodes
 
-def find_alone_Nodes(graph):
-  """функция для нахождения одиночных узлов"""
-  error_nodes = []
-  for node in graph:
-    if len(graph[node]['Children']) == 0 and graph[node]['Parent'] == None:
-      error_nodes.append(node)
-  return error_nodes
+    def find_cycles(self):
+        """метод для нахождения циклов в графе"""
+        G = nx.DiGraph()
+        for node in self.nodes:
+            for child in self.nodes[node].children:
+                G.add_edge(node, child, CLASS=self.nodes[node].category)
+        cycles = list(nx.simple_cycles(G))
+        return cycles
+
+    def draw_graph(self):
+        """метод для визуализации графа с разными цветами для разных категорий"""
+        G = nx.DiGraph()
+        labels = {}
+        colors = {}
+        for node in self.nodes:
+            category = self.nodes[node].category
+            labels[node] = node
+            if category not in colors:
+                colors[category] = (random.random(), random.random(), random.random())
+            G.add_node(node)
+        for node in self.nodes:
+            for child in self.nodes[node].children:
+                G.add_edge(node, child)
+        node_colors = [colors[self.nodes[node].category] for node in self.nodes]
+        pos = nx.spring_layout(G, seed=42)  # добавить атрибут seed
+        sorted_pos = dict(sorted(pos.items()))  # отсортировать словарь pos по ключам
+        nx.draw(G, pos=sorted_pos, with_labels=True, labels=labels, node_color=node_colors, cmap='jet')
+        handles = []
+        for category, color in colors.items():
+            handles.append(plt.scatter([], [], color=color, label=category))
+        plt.legend(handles=handles)
+        plt.show()
+
+    def __str__(self):
+        return f"Graph(nodes={self.nodes})"
+
+    def __repr__(self):
+        return f"Graph({len(self.nodes)} nodes)"
+
 
 if __name__ == '__main__':
-    graph = create_graph(data)
+    graph1 = Graph()
+    graph1.create_from_data(data)
+    print(graph1)
 
-    # выводим граф на экран
-    print("Создан следующий граф:")
-    print(graph)
-
-    print("Найдены следующие циклы:")
-    for cycle in find_cycles(graph):
-        print(cycle)
-
-    print("Ошибка наследования:")
-    print(find_BNodes_to_notBnodes(graph))
-
-    print("Одиночные требования:")
-    print(find_alone_Nodes(graph))
-
-    draw_graph(graph)
+    print(graph1.find_alone_nodes())
+    print(graph1.find_cycles())
+    print(graph1.find_BNodes_to_notBnodes())
+    graph1.draw_graph()
